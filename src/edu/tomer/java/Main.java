96292641d9aab43790bdd0bdb2ec2ade2d96d459
hackerudroids <tomerjava@gmail.com>
2017-03-13 20:50:31 +0200

    BufferedReader

--- src/edu/tomer/java/Main.java
+++ src/edu/tomer/java/Main.java
@@ -1,30 +1,28 @@
 package edu.tomer.java;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        FileReader reader = null;
+        BufferedReader reader = null;
 
         try {
             StringBuilder builder = new StringBuilder();
-            reader = new FileReader("1.txt");
 
-            int read = -1;
-
-            while ((read = reader.read()) != -1) {
-                char c = (char) read;
-                builder.append(c);
+            reader = new BufferedReader(new FileReader("1.txt"));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
             }
             System.out.println(builder.toString());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        }finally {
+            //TODO: close the stream.
         }
 
     }
